356924bc1fbe5a33d822a7558698a22439d6b8db
unknown <christianp1894gmail.com>
2020-07-14 23:36:18 -0500

    Html restful actions

--- Gemfile
+++ Gemfile
@@ -19,6 +19,9 @@ gem 'uglifier', '>= 1.3.0'
 # faker
 gem 'faker'
 
+# rest-client
+gem 'rest-client', '~> 1.8'
+
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Kitten.destroy_all
+
+20.times do
+    Kitten.create(
+        name: Faker::Creature::Cat.name,
+        age: Faker::Number.between(from: 1, to: 20), 
+        cuteness: Faker::Lorem.sentence,
+        softness: Faker::Lorem.sentence
+    )
+end
